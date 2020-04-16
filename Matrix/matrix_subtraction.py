2215032602269de3048904436901782ff221969c
tasbihaasim <54742672+tasbihaasim@users.noreply.github.com>
2020-04-16 15:52:06 +0500

    Add files via upload

---
+++ Matrix/matrix_subtraction.py
@@ -0,0 +1,22 @@
+def subtraction(a, b):
+    if len(a)==len(b):
+        y=[]
+        m=0
+        while m<len(a):
+            c=a[m]-b[m]
+            m=m+1
+            y.append(c)
+    return y
+
+
+def matrix_subtraction(A, B):
+    m=0
+    u=[]
+    if len(A[m])!=len(B[m]):
+        return "Matrices A and B don't have the same dimensions"
+    elif len(A)==len(B):
+        while m<len(A):
+            t=subtraction(A[m],B[m])
+            m=m+1
+            u.append(t)
+        return u
