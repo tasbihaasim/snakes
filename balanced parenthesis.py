51f313a602682bacfeee801d21b18062a0979a30
tasbihaasim <54742672+tasbihaasim@users.noreply.github.com>
2020-04-16 16:02:08 +0500

    Add files via upload
    Braces in a string are considered to be balanced if the following criteria are met:
    All braces must be closed. Braces come in pairs of the form (), {} and [].
    The left brace opens the pair, and the right one closes it.
    In any set of nested braces, the braces between any pair must be closed.
    For example, [{}] is a valid grouping of braces but [}]{} is not.

---
+++ balanced parenthesis.py
@@ -0,0 +1,26 @@
+def isEqual(opening,closing):
+    if( opening == '(' and closing == ')' ):
+        return True;
+    elif ( opening == '[' and closing == ']' ):
+        return True;
+    elif ( opening == '{' and closing == '}' ):
+        return True;
+    return False;
+
+
+def balanced_parenthesis(exp):
+    stack = []
+    for i in range(0 , len(exp)):
+        if (exp[i] == '(' or exp[i] == '{' or exp[i] == '['):
+            stack.insert(0,exp[i])
+            print(stack)
+        elif (exp[i] == ')' or exp[i] == '}' or exp[i] == ']'):
+            if(stack == [] or isEqual(stack[0] , exp[i]) == False ):
+                return False
+            else:
+                stack.pop(0)
+            print(stack)
+    return stack == []
+
+
+
